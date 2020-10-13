64ac2a0bea946bf6d6c6bf22cdff1f7d11a63363
mantunesAmido <marco.antunes@amido.com>
2020-10-13 15:21:26 +0100

    Update UsersTestWithRestSharp.cs
    Removed comments

--- Amido.TechTest.NetCore.XUnit/UsersTestWithRestSharp.cs
+++ Amido.TechTest.NetCore.XUnit/UsersTestWithRestSharp.cs
@@ -19,14 +19,12 @@ namespace Amido.TechTest.NetCore.XUnit
         {
             restClient = new RestClient(baseUrl);
 
-            // add a token and add to the request headers for all future requests
             var token = GetBearerToken();
             restClient.AddDefaultHeader("Authorization", $"Bearer {token}");
         }
 
         public void Dispose()
         {
-            // clean up any user created during tests
             if (userId != "")
             {
                 var request = new RestRequest($"{resource}/{userId}", Method.DELETE);
@@ -39,8 +37,6 @@ namespace Amido.TechTest.NetCore.XUnit
         [Fact]
         public void RestSharp_CreateUser()
         {
-            // use an anonymous type to quickly generate a user object
-            // alternatively you could create a separate class object
             var joe = new
             {
                 name = "Joe",
@@ -53,7 +49,6 @@ namespace Amido.TechTest.NetCore.XUnit
 
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
 
-            // grab the user id from the Location header
             var location = response.Headers.FirstOrDefault(h => h.Name == "Location").Value.ToString();
             userId = new Uri(location).Segments.LastOrDefault();
 
@@ -63,7 +58,6 @@ namespace Amido.TechTest.NetCore.XUnit
         [Fact]
         public void RestSharp_UpdatePassword()
         {
-            // ideally you would have known user to update as this creates a dependancy on user creation
             if (userId == "")
                 RestSharp_CreateUser();
 
@@ -79,14 +73,12 @@ namespace Amido.TechTest.NetCore.XUnit
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            // check the Joe's password was actually updated!
             var joeResponse = JsonConvert.DeserializeAnonymousType(response.Content, joe);
             Assert.Equal(joe.password, joeResponse.password);
         }
 
         private string GetBearerToken()
         {
-            // acquire a bearer token from the /token operation  
             var request = new RestRequest("token");
             var response = restClient.Execute(request);
 

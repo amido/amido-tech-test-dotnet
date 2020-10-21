85fb2058fef9e24d499ac3cb4c797ba0f218805b
mantunesAmido <marco.antunes@amido.com>
2020-10-21 12:06:56 +0100

    doNetCore XBehave examples

---
+++ Amido.TechTest.NetCore.XBehave/UsersTestWithXBehave.cs
@@ -0,0 +1,28 @@
+﻿using Xbehave;
+
+namespace Amido.TechTest.NetCore.XBehave
+{
+    public class UsersTestWithXBehave
+    {
+        private readonly Fixture fixture;
+
+        public UsersTestWithXBehave()
+        {
+            fixture = new Fixture();
+        }
+
+        [Scenario]
+        public void CreateUser()
+        {
+            "Given there is a request to create a new user".x(() => fixture.GivenAUserIsCreated());
+            "Then there should be a user id for the new user".x(() => fixture.ThenAUserIdShouldExist());
+        }
+
+        [Scenario]
+        public void UpdateUser()
+        {
+            "Given there is an existing user".x(() => fixture.GivenAUserIsCreated());
+            "Then a request to update that user's pasword should be successful".x(() => fixture.RequestToUpdateUserIsSuccessful());
+        }
+    }
+}
